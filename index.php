97ec9cd01ce943d196828364893be9c65c57cb90
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2017-12-03 20:29:05 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>dreamt</title>
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<style type="text/css">
+body {
+	background-color: #18232a;
+}
+a:link {
+	color: #4ac2c3;
+	font-family: Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 1.8em; font-style: normal; font-weight: lighter; text-transform:uppercase;
+}
+a:visited {
+	color: #4ac2c3;
+}
+a:hover {
+	color: #4ac2c3;
+}
+a:active {
+	color: #4ac2c3;
+}
+</style>
+<script type="text/javascript">
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+</head>
+
+<body onLoad="MM_preloadImages('images/1a.png','images/2a.png','images/3a.png','images/5a.png','images/6a.png')">
+
+
+<div align="center">
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p><img src="images/dreamt.jpg"  alt="" width="850" height="144" align="middle"/>
+  </p>
+  
+
+
+
+  <p>&nbsp;</p>
+  <p>
+  
+  <a href="http://dreamt.bandcamp.com/album/comfort-road" target="_blank" onMouseOver="MM_swapImage('1','','images/1c.png',1)" onMouseOut="MM_swapImgRestore()"><img src="images/1d.png" alt="reverbnation" width="69" height="68" id="1" style="margin-right:14px;"></a>
+  
+  
+  <a href="http://facebook.com/dreamtmusic" target="_blank" onMouseOver="MM_swapImage('2','','images/2a.png',1)" onMouseOut="MM_swapImgRestore()"><img src="images/2b.png" alt="facebook" width="95" height="66" id="2"></a><a href="http://soundcloud.com/dreamtmusic" target="_blank" onMouseOver="MM_swapImage('3','','images/3a.png',1)" onMouseOut="MM_swapImgRestore()"><img src="images/3b.png" alt="soundcloud" width="148" height="66" id="3"></a><a href="http://twitter.com/TheDreamtBand" target="_blank" onMouseOver="MM_swapImage('4','','images/4a.png',0)" onMouseOut="MM_swapImgRestore()"><img src="images/4b.png" alt="twitter" width="99" height="66" id="4"></a><a href="http://instagram.com/dreamtagram" target="_blank" onMouseOver="MM_swapImage('5','','images/5a.png',1)" onMouseOut="MM_swapImgRestore()"><img src="images/5b.png" alt="instagram" width="103" height="67" id="5"></a></p>
+  <p>&nbsp;</p>
+
+
+ <div style="width:740px;margin-left: auto;
+    margin-right: auto;">
+<iframe src="https://embed.spotify.com/?uri=spotify%3Aalbum%3A2RijGUbCDbWCKsj6bgIEvD"
+width="300" height="380" frameborder="0"
+allowtransparency="true"></iframe>
+
+
+
+
+
+
+<p>&nbsp;</p>
+<iframe src="https://player.vimeo.com/video/92345585" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+<p style="color: #FFF; font-family: Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 12px; font-style: italic; font-weight: bolder;"><br>
+  <span style="font-size: 12px"><a href="mailto:thedreamtband@gmail.com">thedreamtband@gmail.com </a></span></p>
+  <p>&nbsp;</p>
+  
+</div>
+</div>
+</body>
+</html>
